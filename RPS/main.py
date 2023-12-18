bdebde5b7bdbcc012768bdce8ea68d909660eb64
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-12-18 16:39:03 -0600

    rps

--- RPS/main.py
+++ RPS/main.py
@@ -5,15 +5,15 @@ WIDTH = 800
 HEIGHT = 800
 
 
-
-#global variables
-
 # make a list of choices
 brain_choices = ["rock", "paper", "scissors"]
 # get a random selection
 brain = random.choice(brain_choices)
+
+# global variables
 player_choice = ""
 display_text = ""
+next_round = False
 
 # create actors
 paper_btn = Actor("paper", (150, 200))
@@ -23,20 +23,19 @@ scissors_btn = Actor("scissors", (650, 200))
 
 def game_play():
     global display_text
-    display_text = ""
     # this checks if both choices are the same
     if player_choice == brain:
         display_text = "Tie"
     # player choses paper
     elif player_choice == "paper":
         if brain == "rock":
-            display_text = "You win!"
+            display_text = "\nYou win!"
         elif brain == "scissors":
-            display_text = "You lose!"
+            display_text = "\nYou lose!"
     # player chooses rock
     elif player_choice == "rock":
         if brain == "paper":
-            display_text = "You lose"
+            display_text = "\nYou lose"
         elif brain == "scissors":
             display_text = "You win"
     # player chooses scissors
@@ -45,7 +44,9 @@ def game_play():
             display_text = "You win"
         elif brain == "scissors":
             display_text = "You lose"
-        
+    else:
+        display_text = ""
+
 
 # gets called every time update does
 def draw():
@@ -60,6 +61,8 @@ def draw():
 def on_mouse_down(pos):
     global player_choice
     global display_text
+    if next_round:
+        display_text = ""
     if paper_btn.collidepoint(pos):
         player_choice = "paper"
     elif rock_btn.collidepoint(pos):
@@ -68,9 +71,25 @@ def on_mouse_down(pos):
         player_choice = "scissors"
 
 
+
+def display():
+    global display_text
+    global brain
+    global player_choice
+    display_text = f'''
+    Computer chooses: {brain}
+    Player Chooses: {player_choice}
+    '''
+
+def reset():
+    global display_text
+    display_text = ""
+
+
 # gets called 60 times a second
 def update():
-    pass
+    game_play()
+    
 
 
 pgzrun.go()
