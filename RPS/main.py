7c77bd9b7991d7ac5d5d07cfc4ffc5196143d070
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2023-12-18 19:11:28 -0600

    finished rps

--- RPS/main.py
+++ RPS/main.py
@@ -7,13 +7,12 @@ HEIGHT = 800
 
 # make a list of choices
 brain_choices = ["rock", "paper", "scissors"]
-# get a random selection
-brain = random.choice(brain_choices)
 
-# global variables
 player_choice = ""
+brain = ""
 display_text = ""
-next_round = False
+player_score = 0
+com_score = 0
 
 # create actors
 paper_btn = Actor("paper", (150, 200))
@@ -21,35 +20,44 @@ rock_btn = Actor("rock", (400, 200))
 scissors_btn = Actor("scissors", (650, 200))
 
 
-def game_play():
-    global display_text
+def get_winner(player_choice, brain):
+    global player_score
+    global com_score
+    
+    result = ""
     # this checks if both choices are the same
     if player_choice == brain:
-        display_text = "Tie"
+        result = "Tie"
     # player choses paper
     elif player_choice == "paper":
         if brain == "rock":
-            display_text = "\nYou win!"
+            result = "You win!"
+            player_score += 1
         elif brain == "scissors":
-            display_text = "\nYou lose!"
+            result = "You lose!"
+            com_score += 1
     # player chooses rock
     elif player_choice == "rock":
         if brain == "paper":
-            display_text = "\nYou lose"
+            result = "You lose"
+            com_score += 1
         elif brain == "scissors":
-            display_text = "You win"
+            result = "You win"
+            player_score += 1
     # player chooses scissors
     elif player_choice == "scissors":
         if brain == "paper":
-            display_text = "You win"
-        elif brain == "scissors":
-            display_text = "You lose"
-    else:
-        display_text = ""
+            result = "You win"
+            player_score += 1
+        elif brain == "rock":
+            result = "You lose"
+            com_score += 1
+    return result
 
 
 # gets called every time update does
 def draw():
+    screen.clear()
     screen.draw.text("Rock paper scisors", center=(400, 50), fontsize=40, color="brown")
     screen.draw.text(f"{display_text}", center=(400, 600), fontsize=40, color="brown")
     paper_btn.draw()
@@ -59,37 +67,30 @@ def draw():
 
 # gets called when we click any mouse button
 def on_mouse_down(pos):
+    global brain
     global player_choice
-    global display_text
-    if next_round:
-        display_text = ""
+    brain = random.choice(brain_choices)
     if paper_btn.collidepoint(pos):
         player_choice = "paper"
     elif rock_btn.collidepoint(pos):
         player_choice = "rock"
     elif scissors_btn.collidepoint(pos):
         player_choice = "scissors"
-
+    display()
 
 
 def display():
     global display_text
-    global brain
-    global player_choice
     display_text = f'''
     Computer chooses: {brain}
     Player Chooses: {player_choice}
+    
+        {get_winner()}
     '''
-
-def reset():
-    global display_text
-    display_text = ""
 
 
 # gets called 60 times a second
 def update():
-    game_play()
-    
-
+    pass
 
 pgzrun.go()
