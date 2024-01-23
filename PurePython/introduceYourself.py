99fd8182091dc8416bddc57862baa86c4e24252e
JoshuaEnnisDev <joshuaennisdev@gmail.com>
2024-01-22 18:25:12 -0600

    commit

---
+++ PurePython/introduceYourself.py
@@ -0,0 +1,3 @@
+print("Hello")
+name = "Josh"
+print("My name is" + name)
---
+++ PurePython/resturant.py
@@ -0,0 +1,23 @@
+heading = '''
+                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                        @                                         @
+                        @                                         @
+                        @           Josh's Jolly Treats           @
+                        @                                         @
+                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 
+'''
+print(heading)
+
+item_1 = "Vanila Ice Cream"
+item_2 = "Chocolate Ice Cream"
+
+item_1_cost = 3.95
+item_2_cost = 3.59
+
+menu_items =f'''
+                               {item_1}       {item_1_cost}
+                               {item_2}    {item_2_cost}
+'''
+print(menu_items)
+
+input("What would you like to buy? ")
